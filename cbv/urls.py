bd189abf23b126b22ef79e0008acedce5f1fcd69
kusalb <grand.kusal.kb@gmail.com>
2022-03-11 14:46:57 +0545

    New link added

--- cbv/urls.py
+++ cbv/urls.py
@@ -1,13 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from cbv.views import Ex2View
 
 app_name = 'cbv'
 urlpatterns = [
     path('ex1', TemplateView.as_view(template_name='ex1.html', extra_context={'title': 'new title'})),
-    path('ex2', Ex2View.as_view(), name='ex2')
+    path('ex2', Ex2View.as_view(), name='ex2'),
+    path('rdt', RedirectView.as_view(url='https://github.com/Kusalb/cbv'), name='go-to-very')
 
 ]
